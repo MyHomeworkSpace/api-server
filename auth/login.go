a03af5074c57afbcffd82cafd027411d9c908b4f
William B <nywillb@users.noreply.github.com>
2016-09-15 10:41:52 -0400

    Add friendly help messages to the errors.

--- auth/login.go
+++ auth/login.go
@@ -17,7 +17,7 @@ func DaltonLogin(username string, password string) (map[string]interface{}, stri
 
 	req, err := http.NewRequest("POST", "https://hsregistration.dalton.org/src/server/index.phplogin", strings.NewReader(form.Encode()))
 	if err != nil {
-		return nil, "An internal server error occurred while signing you in.", err
+		return nil, "An internal server error occurred while signing you in. (Contact us at hello@myhomework.space for support.)", err
 	}
     req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Add("Referer", "https://hsregistration.dalton.org/")
@@ -25,7 +25,7 @@ func DaltonLogin(username string, password string) (map[string]interface{}, stri
 
     resp, err := hc.Do(req)
 	if err != nil {
-		return nil, "An internal server error occurred while signing you in.", err
+		return nil, "An internal server error occurred while signing you in. (Contact us at hello@myhomework.space for support.)", err
 	}
 
 	defer resp.Body.Close()
@@ -33,10 +33,10 @@ func DaltonLogin(username string, password string) (map[string]interface{}, stri
 
 	var data map[string]interface{}
 	if err := json.Unmarshal([]byte(body), &data); err != nil {
-		return nil, "The response from the Dalton login server was malformed.", err
+		return nil, "The response from the Dalton login server was malformed. (Don't blame us!)", err
     }
 	if data["logged_in"] == false {
-		return nil, "The username or password was incorrect.", nil
+		return nil, "The username or password was incorrect. If you don't know your password, you can visit https://tr.im/1v9f2", nil
 	}
 
 	return data, "", nil
