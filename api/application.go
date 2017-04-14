ede354d438cf9c3eaa1a1b4fd9ccc6d8b4633655
Alex Studer <alex_studer@outlook.com>
2017-04-14 18:39:00 -0400

    add endpoint to revoke application access

--- api/application.go
+++ api/application.go
@@ -13,10 +13,20 @@ type Application struct {
 	AuthorName string `json:"authorName"`
 	CallbackURL string `json:"callbackUrl"`
 }
+type ApplicationAuthorization struct {
+	ID int `json:"id"`
+	ApplicationID int `json:"applicationId"`
+	Name string `json:"name"`
+	AuthorName string `json:"authorName"`
+}
 
 type ApplicationTokenResponse struct {
 	Status string `json:"status"`
 	Token string `json:"token"`
+}
+type ApplicationAuthorizationsResponse struct {
+	Status string `json:"status"`
+	Authorizations []ApplicationAuthorization `json:"authorizations"`
 }
 type SingleApplicationResponse struct {
 	Status string `json:"status"`
@@ -110,11 +120,68 @@ func InitApplicationAPI(e *echo.Echo) {
 		jsonResp := SingleApplicationResponse{"ok", resp}
 		return c.JSON(http.StatusOK, jsonResp)
 	})
-	e.GET("/application/requestAuth/:id", func(c echo.Context) error {
+	e.GET("/application/getAuthorizations", func(c echo.Context) error {
 		if GetSessionUserID(&c) == -1 {
-			jsonResp := ErrorResponse{"error", "logged_out"}
-			return c.JSON(http.StatusUnauthorized, jsonResp)
+			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
 		}
+		rows, err := DB.Query("SELECT application_authorizations.id, applications.id, applications.name, applications.authorName FROM application_authorizations INNER JOIN applications ON application_authorizations.applicationId = applications.id WHERE application_authorizations.userId = ?", GetSessionUserID(&c))
+		if err != nil {
+			log.Println("Error while getting authorizations: ")
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, StatusResponse{"error"})
+		}
+		defer rows.Close()
+
+		authorizations := []ApplicationAuthorization{}
+		for rows.Next() {
+			resp := ApplicationAuthorization{-1, -1, "", ""}
+			rows.Scan(&resp.ID, &resp.ApplicationID, &resp.Name, &resp.AuthorName)
+			authorizations = append(authorizations, resp)
+		}
+		return c.JSON(http.StatusOK, ApplicationAuthorizationsResponse{"ok", authorizations})
+	})
+	e.GET("/application/requestAuth/:id", func(c echo.Context) error {
 		return c.Redirect(http.StatusFound, AuthURLBase + "?id=" + c.Param("id"))
+	})
+	e.POST("/application/revokeAuth", func(c echo.Context) error {
+		if GetSessionUserID(&c) == -1 {
+			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
+		}
+
+		// find the authorization
+		rows, err := DB.Query("SELECT userId FROM application_authorizations WHERE id = ?", c.FormValue("id"))
+		if err != nil {
+			log.Println("Error while revoking authorization:")
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, StatusResponse{"error"})
+		}
+		defer rows.Close()
+
+		if !rows.Next() {
+			return c.JSON(http.StatusForbidden, ErrorResponse{"error", "forbidden"})
+		}
+
+		userId := -1
+		rows.Scan(&userId)
+
+		if GetSessionUserID(&c) != userId {
+			return c.JSON(http.StatusForbidden, ErrorResponse{"error", "forbidden"})
+		}
+
+		// delete the authorization
+		deleteStmt, err := DB.Prepare("DELETE FROM application_authorizations WHERE id = ?")
+		if err != nil {
+			log.Println("Error while revoking authorization:")
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, StatusResponse{"error"})
+		}
+		_, err = deleteStmt.Exec(c.FormValue("id"))
+		if err != nil {
+			log.Println("Error while revoking authorization:")
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, StatusResponse{"error"})
+		}
+
+		return c.JSON(http.StatusOK, StatusResponse{"ok"})
 	})
 }
