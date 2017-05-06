101f54b2463dd113b55cc519dd05e1e273d10e8b
Alex Studer <alex_studer@outlook.com>
2017-05-06 18:44:13 -0400

    add an endpoint to actually get your schedule

--- api/calendar.go
+++ api/calendar.go
@@ -38,14 +38,65 @@ type CalendarPeriod struct {
 	End int `json:"end"`
 	UserID int `json:"userId"`
 }
+type CalendarScheduleItem struct {
+	ID int `json:"id"`
+	TermID int `json:"termId"`
+	ClassID int `json:"classId"`
+	Name string `json:"name"`
+	OwnerID int `json:"ownerId"`
+	OwnerName string `json:"ownerName"`
+	DayNumber int `json:"dayNumber"`
+	Start int `json:"start"`
+	End int `json:"end"`
+	UserID int `json:"userId"`
+}
 
 // responses
+type CalendarScheduleResponse struct {
+	Status string `json:"status"`
+	Terms []CalendarTerm `json:"terms"`
+	Items []CalendarScheduleItem `json:"items"`
+}
 type CalendarStatusResponse struct {
 	Status string `json:"status"`
 	StatusNum int `json:"statusNum"`
 }
 
 func InitCalendarAPI(e *echo.Echo) {
+	e.GET("/calendar/getSchedule", func(c echo.Context) error {
+		if GetSessionUserID(&c) == -1 {
+			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
+		}
+
+		// get the terms
+		terms := []CalendarTerm{}
+		termRows, err := DB.Query("SELECT id, termId, name, userId FROM calendar_terms WHERE userId = ?", GetSessionUserID(&c))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+		}
+		defer termRows.Close()
+		for termRows.Next() {
+			term := CalendarTerm{-1, -1, "", -1}
+			termRows.Scan(&term.ID, &term.TermID, &term.Name, &term.UserID)
+			terms = append(terms, term)
+		}
+
+		// get the periods for each term
+		items := []CalendarScheduleItem{}
+		rows, err := DB.Query("SELECT calendar_periods.id, calendar_classes.termId, calendar_classes.sectionId, calendar_classes.`name`, calendar_classes.ownerId, calendar_classes.ownerName, calendar_periods.dayNumber, calendar_periods.`start`, calendar_periods.`end`, calendar_periods.userId FROM calendar_periods INNER JOIN calendar_classes ON calendar_periods.classId = calendar_classes.sectionId WHERE calendar_periods.userId = ?", GetSessionUserID(&c))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+		}
+		defer rows.Close()
+		for rows.Next() {
+			item := CalendarScheduleItem{-1, -1, -1, "", -1, "", -1, -1, -1, -1}
+			rows.Scan(&item.ID, &item.TermID, &item.ClassID, &item.Name, &item.OwnerID, &item.OwnerName, &item.DayNumber, &item.Start, &item.End, &item.UserID)
+			items = append(items, item)
+		}
+
+		return c.JSON(http.StatusOK, CalendarScheduleResponse{"ok", terms, items})
+	})
+
 	e.GET("/calendar/getStatus", func(c echo.Context) error {
 		if GetSessionUserID(&c) == -1 {
 			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
