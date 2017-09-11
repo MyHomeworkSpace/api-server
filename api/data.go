afe78f84c1c1a62030066c84c474e89a14f8ab33
Alex Studer <astuder@notetoscreen.com>
2017-09-11 17:07:51 -0400

    format data.go

---
+++ api/data.go
@@ -0,0 +1,16 @@
+package api
+
+type User struct {
+	ID                 int    `json:"id"`
+	Name               string `json:"name"`
+	Username           string `json:"username"`
+	Email              string `json:"email"`
+	Type               string `json:"type"`
+	Features           string `json:"features"`
+	Level              int    `json:"level"`
+	ShowMigrateMessage int    `json:"showMigrateMessage"`
+}
+
+func Data_GetUserByID(id int) (User, error) {
+
+}
