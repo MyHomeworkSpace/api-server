ad53ba4db3b4affb30dd9e2aa14f8c150221aa0f
thatoddmailbox <astuder@notetoscreen.com>
2018-09-20 23:15:51 -0400

    fix off-by-one error in friday rotation for first semester schedule imports

--- calendar/consts.go
+++ calendar/consts.go
@@ -25,10 +25,10 @@ var (
 	Term1_Import_Start = time.Date(2018, time.September, 24, 0, 0, 0, 0, time.UTC)
 	Term1_Import_End   = time.Date(2018, time.October, 20, 0, 0, 0, 0, time.UTC)
 
-	Term1_Import_DayOffset_Friday1 = ((7 * 3) + 4)
-	Term1_Import_DayOffset_Friday2 = 4
-	Term1_Import_DayOffset_Friday3 = ((7 * 1) + 4)
-	Term1_Import_DayOffset_Friday4 = ((7 * 2) + 4)
+	Term1_Import_DayOffset_Friday1 = ((7 * 2) + 4)
+	Term1_Import_DayOffset_Friday2 = ((7 * 3) + 4)
+	Term1_Import_DayOffset_Friday3 = 4
+	Term1_Import_DayOffset_Friday4 = ((7 * 1) + 4)
 
 	Term2_Import_Start = time.Date(2019, time.January, 28, 0, 0, 0, 0, time.UTC)
 	Term2_Import_End   = time.Date(2019, time.February, 23, 0, 0, 0, 0, time.UTC)
