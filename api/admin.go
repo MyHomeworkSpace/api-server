ac2865d54ff315102efdba063cda12ae543548d2
Alex Studer <astuder@notetoscreen.com>
2018-11-14 22:53:19 -0500

    change users api to return total count

--- api/admin.go
+++ api/admin.go
@@ -6,42 +6,17 @@ import (
 	"github.com/labstack/echo"
 )
 
-type UsersResponse struct {
-	Status string `json:"status"`
-	Users  []User `json:"users"`
-}
-
 type FeedbacksResponse struct {
 	Status    string     `json:"status"`
 	Feedbacks []Feedback `json:"feedbacks"`
 }
 
+type UserCountResponse struct {
+	Status string `json:"status"`
+	Count  int    `json:"count"`
+}
+
 func InitAdminAPI(e *echo.Echo) {
-	e.GET("/admin/getAllUsers", func(c echo.Context) error {
-		if GetSessionUserID(&c) == -1 {
-			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
-		}
-		user, _ := Data_GetUserByID(GetSessionUserID(&c))
-		if user.Level < 1 {
-			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "forbidden"})
-		}
-
-		rows, err := DB.Query("SELECT ID, name, username, email, type, features, level, showMigrateMessage FROM users")
-		if err != nil {
-			ErrorLog_LogError("getting all users", err)
-			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-		}
-
-		users := []User{}
-		for rows.Next() {
-			resp := User{-1, "", "", "", "", "", -1, -1}
-			rows.Scan(&resp.ID, &resp.Name, &resp.Username, &resp.Email, &resp.Type, &resp.Features, &resp.Level, &resp.ShowMigrateMessage)
-			users = append(users, resp)
-		}
-
-		return c.JSON(http.StatusOK, UsersResponse{"ok", users})
-	})
-
 	e.GET("/admin/getAllFeedback", func(c echo.Context) error {
 		if GetSessionUserID(&c) == -1 {
 			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
@@ -65,5 +40,27 @@ func InitAdminAPI(e *echo.Echo) {
 		}
 
 		return c.JSON(http.StatusOK, FeedbacksResponse{"ok", feedbacks})
+	})
+
+	e.GET("/admin/getUserCount", func(c echo.Context) error {
+		if GetSessionUserID(&c) == -1 {
+			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
+		}
+		user, _ := Data_GetUserByID(GetSessionUserID(&c))
+		if user.Level < 1 {
+			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "forbidden"})
+		}
+
+		rows, err := DB.Query("SELECT COUNT(*) FROM users")
+		if err != nil {
+			ErrorLog_LogError("getting user count", err)
+			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+		}
+
+		rows.Next()
+		count := -1
+		rows.Scan(&count)
+
+		return c.JSON(http.StatusOK, UserCountResponse{"ok", count})
 	})
 }
