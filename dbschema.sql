04d5771e429ee5f9a1bed9bcbb0400680a4b6f0f
William B <william@barkoffusa.com>
2019-03-09 22:13:28 -0500

    Forgot to add new dbschema, sorry

--- dbschema.sql
+++ dbschema.sql
@@ -213,5 +213,7 @@ CREATE TABLE `users` (
   `canFeedback` tinyint(1) NOT NULL DEFAULT '0',
   `canAnnouncements` tinyint(1) NOT NULL DEFAULT '0',
   `showMigrateMessage` int(11) DEFAULT NULL,
+  `twoFactorSecret` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `twoFactorVerified` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
