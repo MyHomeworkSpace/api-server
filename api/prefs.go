219020f34e23fb8f69328a8938148e26016fedb8
Alex Studer <alex@studer.dev>
2019-05-17 18:08:38 -0400

    refactor prefs api file

--- api/prefs.go
+++ api/prefs.go
@@ -19,70 +19,84 @@ type PrefResponse struct {
 	ReturnedPref Pref   `json:"pref"`
 }
 
+func routePrefsGet(w http.ResponseWriter, r *http.Request, ec echo.Context, c RouteContext) {
+	if GetSessionUserID(&ec) == -1 {
+		ec.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
+		return
+	}
+
+	rows, err := DB.Query("SELECT `id`, `key`, `value` FROM prefs WHERE userId = ? AND `key` = ?", GetSessionUserID(&ec), ec.Param("key"))
+	if err != nil {
+		ErrorLog_LogError("getting pref", err)
+		ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+		return
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		ec.JSON(http.StatusNotFound, ErrorResponse{"error", "not_found"})
+		return
+	}
+
+	resp := Pref{-1, "", ""}
+	rows.Scan(&resp.ID, &resp.Key, &resp.Value)
+
+	ec.JSON(http.StatusOK, PrefResponse{"ok", resp})
+}
+
+func routePrefsSet(w http.ResponseWriter, r *http.Request, ec echo.Context, c RouteContext) {
+	if GetSessionUserID(&ec) == -1 {
+		ec.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
+		return
+	}
+
+	if ec.FormValue("key") == "" || ec.FormValue("value") == "" {
+		ec.JSON(http.StatusBadRequest, ErrorResponse{"error", "missing_params"})
+		return
+	}
+
+	rows, err := DB.Query("SELECT `id`, `key`, `value` FROM prefs WHERE userId = ? AND `key` = ?", GetSessionUserID(&ec), ec.FormValue("key"))
+	if err != nil {
+		ErrorLog_LogError("setting pref", err)
+		ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+		return
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		// doesn't exist, add it
+		stmt, err := DB.Prepare("INSERT INTO prefs(userId, `key`, `value`) VALUES(?, ?, ?)")
+		if err != nil {
+			ErrorLog_LogError("inserting pref", err)
+			ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+			return
+		}
+		_, err = stmt.Exec(GetSessionUserID(&ec), ec.FormValue("key"), ec.FormValue("value"))
+		if err != nil {
+			ErrorLog_LogError("inserting pref", err)
+			ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+			return
+		}
+	} else {
+		// exists already, update it
+		stmt, err := DB.Prepare("UPDATE prefs SET `key` = ?, `value` = ? WHERE userId = ? AND `key` = ?")
+		if err != nil {
+			ErrorLog_LogError("updating pref", err)
+			ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+			return
+		}
+		_, err = stmt.Exec(ec.FormValue("key"), ec.FormValue("value"), GetSessionUserID(&ec), ec.FormValue("key"))
+		if err != nil {
+			ErrorLog_LogError("updating pref", err)
+			ec.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
+			return
+		}
+	}
+
+	ec.JSON(http.StatusOK, StatusResponse{"ok"})
+}
+
 func InitPrefsAPI(e *echo.Echo) {
-	e.GET("/prefs/get/:key", func(c echo.Context) error {
-		if GetSessionUserID(&c) == -1 {
-			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
-		}
-
-		rows, err := DB.Query("SELECT `id`, `key`, `value` FROM prefs WHERE userId = ? AND `key` = ?", GetSessionUserID(&c), c.Param("key"))
-		if err != nil {
-			ErrorLog_LogError("getting pref", err)
-			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-		}
-		defer rows.Close()
-
-		if !rows.Next() {
-			return c.JSON(http.StatusNotFound, ErrorResponse{"error", "not_found"})
-		}
-
-		resp := Pref{-1, "", ""}
-		rows.Scan(&resp.ID, &resp.Key, &resp.Value)
-
-		return c.JSON(http.StatusOK, PrefResponse{"ok", resp})
-	})
-
-	e.POST("/prefs/set", func(c echo.Context) error {
-		if GetSessionUserID(&c) == -1 {
-			return c.JSON(http.StatusUnauthorized, ErrorResponse{"error", "logged_out"})
-		}
-		if c.FormValue("key") == "" || c.FormValue("value") == "" {
-			return c.JSON(http.StatusBadRequest, ErrorResponse{"error", "missing_params"})
-		}
-
-		rows, err := DB.Query("SELECT `id`, `key`, `value` FROM prefs WHERE userId = ? AND `key` = ?", GetSessionUserID(&c), c.FormValue("key"))
-		if err != nil {
-			ErrorLog_LogError("setting pref", err)
-			return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-		}
-		defer rows.Close()
-
-		if !rows.Next() {
-			// doesn't exist, add it
-			stmt, err := DB.Prepare("INSERT INTO prefs(userId, `key`, `value`) VALUES(?, ?, ?)")
-			if err != nil {
-				ErrorLog_LogError("inserting pref", err)
-				return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-			}
-			_, err = stmt.Exec(GetSessionUserID(&c), c.FormValue("key"), c.FormValue("value"))
-			if err != nil {
-				ErrorLog_LogError("inserting pref", err)
-				return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-			}
-			return c.JSON(http.StatusOK, StatusResponse{"ok"})
-		} else {
-			// exists already, update it
-			stmt, err := DB.Prepare("UPDATE prefs SET `key` = ?, `value` = ? WHERE userId = ? AND `key` = ?")
-			if err != nil {
-				ErrorLog_LogError("updating pref", err)
-				return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-			}
-			_, err = stmt.Exec(c.FormValue("key"), c.FormValue("value"), GetSessionUserID(&c), c.FormValue("key"))
-			if err != nil {
-				ErrorLog_LogError("updating pref", err)
-				return c.JSON(http.StatusInternalServerError, ErrorResponse{"error", "internal_server_error"})
-			}
-			return c.JSON(http.StatusOK, StatusResponse{"ok"})
-		}
-	})
+	e.GET("/prefs/get/:key", Route(routePrefsGet))
+	e.POST("/prefs/set", Route(routePrefsSet))
 }
